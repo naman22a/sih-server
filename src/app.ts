a00d55498b2a774e054970bdd94811ca60e674a1
Naman Arora <namanarora1022@gmail.com>
2023-09-20 18:56:43 +0530

    feat: cors origin

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = parseInt(process.env.PORT ?? '5000', 10);
@@ -7,6 +8,7 @@ const port = parseInt(process.env.PORT ?? '5000', 10);
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: '*' }));
 
 // ROUTES
 
